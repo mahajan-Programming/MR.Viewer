eda8e9f5c3ae1bac91c5b796b467cfad07daeb5e
mahajan-programming <shreyas.official07@gmail.com>
2022-07-05 20:46:52 +0530

    project added

---
+++ MrViewer/MrViewer/src/main/java/com/virtusa/MrViewer/MrViewer/MrViewerApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.MrViewer.MrViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrViewerApplication.class, args);
+	}
+
+}
---
+++ MrViewer/MrViewer/src/test/java/com/virtusa/MrViewer/MrViewer/MrViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.MrViewer.MrViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
